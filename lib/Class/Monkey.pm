62a71685b24fb176158605a355b06e9b66f55561
Brad Haywood <brad@perlpowered.com>
2012-04-24 23:21:17 +0100

    Removed debug crap

--- lib/Class/Monkey.pm
+++ lib/Class/Monkey.pm
@@ -153,7 +153,6 @@ Please see C<tweak> for more information on how to get this method. C<haz> behav
 sub haz {
     my (@args) = @_;
     my $pkg = getscope();
-    print "Pushing to $pkg\n";
     if (scalar @args > 0) {
         for my $m (@args) {
             push @{$Class::Monkey::CanPatch}, $m;
